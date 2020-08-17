f382c1f09697c32497b9ef2337e4689dbc3b2a92
gazzamc <gazzamc@live.com>
2020-08-17 22:46:00 +0100

    Added basic structure to app, Added to ReadMe

---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './header.module.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Header</h1>
+      </div>
+    );
+  }
+}
---
+++ src/components/upload/upload.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './upload.module.css';
+
+export default class Upload extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Upload</h1>
+      </div>
+    );
+  }
+}
